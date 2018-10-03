1f6c99bf9d690cafa0a91817e27d364ba7a21c01
khaledhoues <khaledhoues@yahoo.fr>
2018-10-04 00:09:43 +0900

    add viewpager and tabs (change to fragments)

--- app/src/main/java/com/khaledhoues/movies/activities/MainActivity.java
+++ app/src/main/java/com/khaledhoues/movies/activities/MainActivity.java
@@ -1,42 +1,24 @@
 package com.khaledhoues.movies.activities;
 
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.widget.ImageView;
 
+import com.gigamole.navigationtabstrip.NavigationTabStrip;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 import com.khaledhoues.movies.R;
-import com.khaledhoues.movies.adapters.CustomRecyclerViewAdapter;
-import com.khaledhoues.movies.entities.Article;
-import com.khaledhoues.movies.utils.SharedInformation;
-import com.khaledhoues.movies.viewmodels.ArticlesViewModel;
+import com.khaledhoues.movies.adapters.CustomPagerAdapter;
 
-import java.util.ArrayList;
-import java.util.List;
+public class MainActivity extends FragmentActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-    private ArticlesViewModel mArticleViewModel;
-
-    private RecyclerView mRecyclerView;
-    private CustomRecyclerViewAdapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     private InterstitialAd mInterstitialAd;
+    private NavigationTabStrip navigationTabStrip;
 
 
     @Override
@@ -56,13 +38,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAdLoaded() {
                 // Code to be executed when an ad finishes loading.
-                Log.e("AD", "onAdLoaded: LOADED" );
+                Log.e("AD", "onAdLoaded: LOADED");
             }
 
             @Override
             public void onAdFailedToLoad(int errorCode) {
                 // Code to be executed when an ad request fails.
-                Log.e("AD", "onAdFailedToLoad: "+errorCode );
+                Log.e("AD", "onAdFailedToLoad: " + errorCode);
             }
 
             @Override
@@ -83,68 +65,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mArticleViewModel = ViewModelProviders.of(this).get(ArticlesViewModel.class);
+        NavigationTabStrip navigationTabStrip = (NavigationTabStrip) findViewById(R.id.nts_top);
+        navigationTabStrip.setTabIndex(0, true);
 
+//        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+//        viewPager.setAdapter(new CustomPagerAdapter(this));
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
-        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        ViewPager vpPager = (ViewPager) findViewById(R.id.viewpager);
+        CustomPagerAdapter adapterViewPager = new CustomPagerAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
 
+        navigationTabStrip.setViewPager(vpPager);
 
-        mLayoutManager = new LinearLayoutManager(this);
-        setRecyclerViewLayoutManager();
+//        getSupportFragmentManager().beginTransaction()
+//                .add(R.id.content, new NewsFragment(), "News")
+//                .addToBackStack("AppFrags")
+//                .commit();
 
-
-        mAdapter = new CustomRecyclerViewAdapter(new ArrayList<Article>(), new CustomRecyclerViewAdapter.ArticleItemClickListener() {
-            @Override
-            public void onArticleItemClick(int adapterPosition, Article article, ImageView imgThumbnail) {
-                Intent intent = new Intent(MainActivity.this, ArticleDetailsActivity.class);
-//                intent.putExtra("Article",  article.getImage());
-                SharedInformation.setSharedArticle(article);
-                intent.putExtra("TransitionName", "thumbnailTransition");
-
-                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
-                        MainActivity.this,
-                        imgThumbnail,
-                        "thumbnailTransition");
-
-                startActivity(intent, options.toBundle());
-            }
-        });
-
-        mRecyclerView.setAdapter(mAdapter);
-
-        mArticleViewModel.getAllArticles().observe(this, new Observer<List<Article>>() {
-            @Override
-            public void onChanged(@Nullable final List<Article> articles) {
-                mAdapter.setArticles(articles);
-                mSwipeRefreshLayout.setRefreshing(false);
-            }
-        });
-
-        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                mArticleViewModel.refreshArticlesList();
-            }
-        });
-
-
-    }
-
-
-    public void setRecyclerViewLayoutManager() {
-        int scrollPosition = 0;
-
-        // If a layout manager has already been set, get current scroll position.
-        if (mRecyclerView.getLayoutManager() != null) {
-            scrollPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager())
-                    .findFirstCompletelyVisibleItemPosition();
-        }
-
-
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.scrollToPosition(scrollPosition);
     }
 
 
---
+++ app/src/main/java/com/khaledhoues/movies/fragments/NewsFragment.java
@@ -0,0 +1,146 @@
+package com.khaledhoues.movies.fragments;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.khaledhoues.movies.R;
+import com.khaledhoues.movies.activities.ArticleDetailsActivity;
+import com.khaledhoues.movies.adapters.CustomRecyclerViewAdapter;
+import com.khaledhoues.movies.entities.Article;
+import com.khaledhoues.movies.utils.SharedInformation;
+import com.khaledhoues.movies.viewmodels.ArticlesViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class NewsFragment extends Fragment {
+    private ArticlesViewModel mArticleViewModel;
+
+    private RecyclerView mRecyclerView;
+    private CustomRecyclerViewAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
+
+    public NewsFragment() {
+        // Required empty public constructor
+
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+
+        View rootView = inflater.inflate(R.layout.fragment_news, container, false);
+
+        mArticleViewModel = ViewModelProviders.of(this).get(ArticlesViewModel.class);
+
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swiperefresh);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        setRecyclerViewLayoutManager();
+
+
+        mAdapter = new CustomRecyclerViewAdapter(new ArrayList<Article>(), new CustomRecyclerViewAdapter.ArticleItemClickListener() {
+            @Override
+            public void onArticleItemClick(int adapterPosition, Article article, ImageView imgThumbnail) {
+                Intent intent = new Intent(getActivity(), ArticleDetailsActivity.class);
+//                intent.putExtra("Article",  article.getImage());
+                SharedInformation.setSharedArticle(article);
+                intent.putExtra("TransitionName", "thumbnailTransition");
+
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        getActivity(),
+                        imgThumbnail,
+                        "thumbnailTransition");
+
+                startActivity(intent, options.toBundle());
+            }
+        });
+
+        mRecyclerView.setAdapter(mAdapter);
+
+
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mArticleViewModel.refreshArticlesList();
+            }
+        });
+
+        mSwipeRefreshLayout.setRefreshing(true);
+        mArticleViewModel.getAllArticles().observe(this, new Observer<List<Article>>() {
+            @Override
+            public void onChanged(@Nullable final List<Article> articles) {
+                if (mAdapter != null && mSwipeRefreshLayout != null) {
+                    if (articles != null && articles.size() > 0 && !articles.get(0).getTitle().isEmpty()) {
+                        mAdapter.setArticles(articles);
+                        mRecyclerView.setVisibility(View.VISIBLE);
+                        mSwipeRefreshLayout.setRefreshing(false);
+                    }
+                }
+
+            }
+        });
+
+        return rootView;
+    }
+
+    public void onButtonPressed(Uri uri) {
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    public void setRecyclerViewLayoutManager() {
+        int scrollPosition = 0;
+
+        // If a layout manager has already been set, get current scroll position.
+        if (mRecyclerView.getLayoutManager() != null) {
+            scrollPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }
+
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.scrollToPosition(scrollPosition);
+    }
+
+}
